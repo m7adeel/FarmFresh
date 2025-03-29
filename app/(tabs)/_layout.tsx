49a2436113712dc336478a10b815504eeac2b560
Muhammad Adeel <m7adeel@gmail.com>
2025-03-29 10:50:59 -0700

    fix home logo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Chrome as Home, Store, ShoppingBag, User } from 'lucide-react-native';
+import { Home, Store, ShoppingBag, User } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
